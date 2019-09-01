54fb0ef4561dccaa2c72c7c9c0b70556967a036e
Mark A. Donohoe <Mark@MarkDonohoe.com>
2019-09-01 16:36:16 -0400

    Updated default sites

--- omnikey.safariextension/js/defaults.js
+++ omnikey.safariextension/js/defaults.js
@@ -3,51 +3,58 @@ var Omnikey = Omnikey || {};
 Omnikey.STORAGE_KEY = 'sites';
 Omnikey.default_sites = [
   {
-    key: "amazon",
-    url: "http://www.amazon.com/s/?tag=httpmariec-20&link_code=wsw&_encoding=UTF-8&search-alias=aps&field-keywords={search}&Submit.x=0&Submit.y=0&Submit=Go"
+    key         : "az",
+    description : "Amazon Product Search",
+    url         : "https://www.amazon.com/s?k={search}"
   },
   {
-    key: "youtube",
-    url: "http://www.youtube.com/results?search_query={search}"
+    key         : "azo",
+    description : "Amazon Order List/Search",
+    url         : "https://www.amazon.com/gp/your-account/order-history?search={search}"
   },
   {
-    key: "twitter",
-    url: "https://twitter.com/search?q={search}"
+    key         : "yt",
+    description : "YouTube",
+    url         : "http://www.youtube.com/results?search_query={search}"
   },
   {
-    key: "spotify",
-    url: "https://open.spotify.com/search/results/{%search}"
+    key         : "spot",
+    description : "Spotify",
+    url         : "https://open.spotify.com/search/results/{%search}"
   },
   {
-    key: "ebay",
-    url: "http://www.ebay.com/sch/i.html?_trksid=p5197.m570.l1313&_nkw={search}&_sacat=0"
+    key         : "ebay",
+    description : "eBay",
+    url         : "https://www.ebay.com/sch/i.html?_nkw={search}"
   },
   {
-    key: "wiki",
-    url: "http://en.wikipedia.org/w/index.php?title=Special:Search&search={search}"
+    key         : "wiki",
+    description : "Wikipedia",
+    url         : "https://en.wikipedia.org/wiki/Special:Search?search={search}"
   },
   {
-    key: "zappos",
-    url: "http://www.zappos.com/search?term={search}"
+    key         : "gm",
+    description : "Map Location",
+    url         : "https://www.google.com/maps/search/{search}"
   },
   {
-    key: "maps",
-    url: "https://maps.google.com/maps?hl=en&authuser=0&q={search}&ie=UTF-8"
+    key         : "gd",
+    description : "Google Directions",
+    url         : "https://www.google.com/maps/dir/41-21+28th+St,+Long+Island+City,+NY+11101,+USA/{search}"
   },
   {
-    key: "rt",
-    url: "http://www.rottentomatoes.com/search/?search={search}"
+    key         : "rt",
+    description : "Rotten Tomatoes",
+    url         : "http://www.rottentomatoes.com/search/?search={search}"
   },
   {
-    key: "imdb",
-    url: "http://www.imdb.com/find?q={search}&s=all"
+    key         : "imdb",
+    description : "IMDB",
+    url         : "http://www.imdb.com/find?q={search}&s=all"
   },
   {
-    key: "ddg",
-    url: "http://duckduckgo.com/?q={search}"
-  },
-  {
-    key: "wolfram",
-    url: "http://www.wolframalpha.com/input/?i={search}"
+    key         : "mc",
+    description : "Metacritic",
+    url         : "https://www.metacritic.com/search/all/{search}/results"
   }
 ];
